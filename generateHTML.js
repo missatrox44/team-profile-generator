d9e718b72c1396368d82c1f7a31124b0c255e4ed
Sara Baqla <missatrox44@gmail.com>
2022-07-19 20:41:05 -0500

    created question arrays

---
+++ generateHTML.js
@@ -0,0 +1,8 @@
+function generateHTML(data) {
+  return `
+
+  
+  `;
+}
+
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -16,10 +16,89 @@ const fs = require('fs');
 
 let responses = [];
 
-const EmployeeQuestions = [
+//engineer question array
+const engineerQuestions = [
   {
     type: 'input',
     name: 'name',
     message: 'What is your name?'
-  }
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID number?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your github username?'
+  },
+
 ]
+
+//intern question array
+const internQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID number?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?'
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: 'What school did you attend?'
+  },
+
+]
+
+//manager question array
+const managerQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your ID number?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?'
+  },
+  {
+    type: 'input',
+    name: 'officeNum',
+    message: 'What is your office number?'
+  },
+
+]
+
+//use fs to make index.html
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// function to initialize program
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses)=>{
+      writeToFile("index.html", generateMarkdown({...inquirerResponses}))
+  })
+}
